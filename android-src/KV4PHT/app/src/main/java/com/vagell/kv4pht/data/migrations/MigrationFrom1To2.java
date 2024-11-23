437ba793ba67a2b98c629bcec17822ac6790583c
Vance Vagell <vance@vagell.com>
2024-11-22 21:04:21 -0500

    Fix issue #112 by updating the DB to change the memory format to include the expected extra 0 of frequency precision. Also make text in APRS messages selectable.

--- android-src/KV4PHT/app/src/main/java/com/vagell/kv4pht/data/migrations/MigrationFrom1To2.java
+++ android-src/KV4PHT/app/src/main/java/com/vagell/kv4pht/data/migrations/MigrationFrom1To2.java
@@ -1,3 +1,21 @@
+/*
+kv4p HT (see http://kv4p.com)
+Copyright (C) 2024 Vance Vagell
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.vagell.kv4pht.data.migrations;
 
 import androidx.room.migration.Migration;
@@ -31,5 +49,8 @@ public class MigrationFrom1To2 extends Migration {
                 "`wind_force` INTEGER NOT NULL, " +
                 "`wind_dir` TEXT" +
                 ")");
+
+        // update frequencies in channel_memories from 'xxx.xxx' to 'xxx.xxxx' (ESP32 firmware v2+ requires this new format)
+        database.execSQL("UPDATE channel_memories SET frequency = frequency || '0' WHERE length(frequency) = 7");
     }
 }
