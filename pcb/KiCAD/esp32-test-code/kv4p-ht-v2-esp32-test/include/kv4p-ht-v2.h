dab593f3f312d49a2da0f4c58de9162ce4daadd6
Mark Smith <mark-github@halibut.com>
2024-12-06 08:04:36 -0800

    [PCB] v2.0c (#129)
    * Small test program to validate v2 hardware. #88
    
    * v2.1 work. I think I'm done, waiting for comments from Vance. #88
    
    * Panelizing to meet JLCPCB's minimum 70x70mm dimensions. #88
    
    * A small board to test the LPF design. #88
    
    * Moved from Filter\ Test, so I can also make a UHF filter test. #22
    
    * Split Filter\ Test into VHF and UHF versions. Order VHF yesterday, UHF today. #22
    
    * De-bumping to v2.1.  Sticking with v2.0c instead. #88
    
    * v2.0c release candidate. I'm about to order this.
    
    * Panelized for JLCPCB production. (They need 70x70mm.) #88
    
    * VNA measurements of LPF test boards.  #88 and #22
    
    * Thanks to @jh4vsm for finding this bug.  https://github.com/VanceVagell/kv4p-ht/pull/129#issuecomment-2522023437 #88 #129

---
+++ pcb/KiCAD/esp32-test-code/kv4p-ht-v2-esp32-test/include/kv4p-ht-v2.h
@@ -0,0 +1,29 @@
+/*
+ * Defines for all the pins, where hardware is attached.
+ */
+
+#define PIN_BUTTON_PROGRAM (0)
+#define PIN_TX_TO_USB (1)
+#define PIN_STOCK_LED (2)
+#define PIN_RX_FROM_USB (3)
+#define PIN_RADIO_SQUELCH (4)
+#define PIN_BUTTON_PTT_RIGHT (5)
+#define PIN_GPIOHEAD_2 (12)
+#define PIN_NEOPIXEL (13)
+#define PIN_GPIOHEAD_4 (14)
+#define PIN_GPIOHEAD_6 (15)
+#define PIN_RX_FROM_RADIO (16)
+#define PIN_TX_TO_RADIO (17)
+#define PIN_RADIO_PTT (18)
+#define PIN_RADIO_PD (19)
+#define PIN_SAOHEAD_SDA (21)
+#define PIN_SAOHEAD_SCK (22)
+#define PIN_RADIO_HI_LOW (23)
+#define PIN_AUDIO_TX_TO_RADIO (25)
+#define PIN_SAOHEAD_5 (26)
+#define PIN_SAOHEAD_6 (27)
+#define PIN_GPIOHEAD_7 (32)
+#define PIN_BUTTON_PTT_LEFT (33)
+#define PIN_AUDIO_RX_FROM_RADIO (34)
+#define PIN_GPIOHEAD_8 (35)
+
