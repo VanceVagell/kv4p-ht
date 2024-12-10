f96d9756b587e719d81f059aa4d2084e326060ef
Mark Smith <mark-github@halibut.com>
2024-12-10 02:00:25 -0800

    Fix inductor sizes, more sturdy PTT switches, clarify silk screen around NeoPixel, split Panel out into its own production/ directory. (#131)
    * Small test program to validate v2 hardware. #88
    
    * v2.1 work. I think I'm done, waiting for comments from Vance. #88
    
    * Panelizing to meet JLCPCB's minimum 70x70mm dimensions. #88
    
    * A small board to test the LPF design. #88
    
    * Moved from Filter\ Test, so I can also make a UHF filter test. #22
    
    * Split Filter\ Test into VHF and UHF versions. Order VHF yesterday, UHF today. #22
    
    * De-bumping to v2.1.  Sticking with v2.0c instead. #88
    
    * v2.0c release candidate. I'm about to order this.
    
    * Panelized for JLCPCB production. (They need 70x70mm.) #88
    
    * VNA measurements of LPF test boards.  #88 and #22
    
    * Thanks to @jh4vsm for finding this bug.  https://github.com/VanceVagell/kv4p-ht/pull/129#issuecomment-2522023437 #88 #129
    
    * Ignoring lock and tmp files.
    
    * MFG issues. 2012mm inductors are actually 2115mm. PTT buttons now through hole. Added + indicator on NeoPixel. #88 These production/ files are single board.
    
    * Moved panel to its own project directory. #130
    
    * Ignore vim swap files
    
    * Uses json now, same data source as running from KiCAD. #130
    
    * Moved the 2115 inductor outline into the shared library. #130
    
    * Didn't save before previous commit. Oops. #131

---
+++ pcb/KiCAD/kv4p-ht-PANEL/panelize.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kikit panelize -p ./panelize.json  ../kv4p-ht/kv4p-ht.kicad_pcb ./kv4p-ht-PANEL.kicad_pcb
--- pcb/KiCAD/kv4p-ht/panelize.sh
+++
@@ -1,9 +0,0 @@
-kikit panelize \
-    --layout 'hspace: 5mm; cols: 2' \
-    --tabs 'type: fixed; vwidth: 20mm; hwidth: 10mm; hcount: 0' \
-    --cuts 'type: vcuts; clearance: 1mm; layer: Edge.Cuts' \
-    --framing 'type: railstb' \
-    --tooling 'type: 4hole; hoffset: 5mm; voffset: 2.5mm' \
-    --fiducials 'type: 4fid; hoffset: 10mm; voffset: 2.5mm' \
-    --text 'type: simple; voffset: 2.5mm; text: kv4p-ht v2.1a 2024-11-24 Halibut Electronics' \
-    './kv4p-ht.kicad_pcb' panel.kicad_pcb
