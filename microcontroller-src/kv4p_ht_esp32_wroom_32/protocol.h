7d9da5ce6c8c4a104991bcb855866d523bd1b7dd
Dmitry Kaukov <dkaukov@opentable.com>
2025-02-28 21:02:01 +1100

    cleanup

--- microcontroller-src/kv4p_ht_esp32_wroom_32/protocol.h
+++ microcontroller-src/kv4p_ht_esp32_wroom_32/protocol.h
@@ -17,28 +17,28 @@ const uint8_t COMMAND_STOP             = 5;  // stop everything, just wait for n
 const uint8_t COMMAND_GET_FIRMWARE_VER = 6;  // report FIRMWARE_VER in the format '00000001' for 1 (etc.)
 
 // Outgoing commands (ESP32 -> Android)
-const byte COMMAND_SMETER_REPORT  = 0x53; // 'S'
-const byte COMMAND_PHYS_PTT_DOWN  = 0x44; // 'D'
-const byte COMMAND_PHYS_PTT_UP    = 0x55; // 'U'
-const byte COMMAND_DEBUG_INFO     = 0x01;
-const byte COMMAND_DEBUG_ERROR    = 0x02;
-const byte COMMAND_DEBUG_WARN     = 0x03;
-const byte COMMAND_DEBUG_DEBUG    = 0x04;
-const byte COMMAND_DEBUG_TRACE    = 0x05;
-const byte COMMAND_HELLO          = 0x06;
-const byte COMMAND_RX_AUDIO       = 0x07;
-const byte COMMAND_VERSION        = 0x08;
+const uint8_t COMMAND_SMETER_REPORT  = 0x53; // 'S'
+const uint8_t COMMAND_PHYS_PTT_DOWN  = 0x44; // 'D'
+const uint8_t COMMAND_PHYS_PTT_UP    = 0x55; // 'U'
+const uint8_t COMMAND_DEBUG_INFO     = 0x01;
+const uint8_t COMMAND_DEBUG_ERROR    = 0x02;
+const uint8_t COMMAND_DEBUG_WARN     = 0x03;
+const uint8_t COMMAND_DEBUG_DEBUG    = 0x04;
+const uint8_t COMMAND_DEBUG_TRACE    = 0x05;
+const uint8_t COMMAND_HELLO          = 0x06;
+const uint8_t COMMAND_RX_AUDIO       = 0x07;
+const uint8_t COMMAND_VERSION        = 0x08;
 
+// COMMAND_VERSION parameters
 struct version {
     uint16_t    ver;
     char        radioModuleStatus;
     hw_ver_t    hw;
 } __attribute__((__packed__));
-  
 typedef struct version Version;
 
+// COMMAND_SMETER_REPORT parameters
 struct rssi {
     uint8_t     val;
 } __attribute__((__packed__));
-  
 typedef struct rssi Rssi;
