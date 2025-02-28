edeb350bf36770545ad9390d894df80258ce598f
Dmitry Kaukov <dkaukov@opentable.com>
2025-02-28 21:02:58 +1100

    cleanup

--- microcontroller-src/kv4p_ht_esp32_wroom_32/protocol.h
+++ microcontroller-src/kv4p_ht_esp32_wroom_32/protocol.h
@@ -14,20 +14,20 @@ const uint8_t COMMAND_PTT_UP           = 2;  // stop transmitting audio, go into
 const uint8_t COMMAND_TUNE_TO          = 3;  // change the frequency
 const uint8_t COMMAND_FILTERS          = 4;  // toggle filters on/off
 const uint8_t COMMAND_STOP             = 5;  // stop everything, just wait for next command
-const uint8_t COMMAND_GET_FIRMWARE_VER = 6;  // report FIRMWARE_VER in the format '00000001' for 1 (etc.)
+const uint8_t COMMAND_GET_FIRMWARE_VER = 6;  // reply with [COMMAND_VERSION(Version)]
 
 // Outgoing commands (ESP32 -> Android)
-const uint8_t COMMAND_SMETER_REPORT  = 0x53; // 'S'
-const uint8_t COMMAND_PHYS_PTT_DOWN  = 0x44; // 'D'
-const uint8_t COMMAND_PHYS_PTT_UP    = 0x55; // 'U'
-const uint8_t COMMAND_DEBUG_INFO     = 0x01;
-const uint8_t COMMAND_DEBUG_ERROR    = 0x02;
-const uint8_t COMMAND_DEBUG_WARN     = 0x03;
-const uint8_t COMMAND_DEBUG_DEBUG    = 0x04;
-const uint8_t COMMAND_DEBUG_TRACE    = 0x05;
-const uint8_t COMMAND_HELLO          = 0x06;
-const uint8_t COMMAND_RX_AUDIO       = 0x07;
-const uint8_t COMMAND_VERSION        = 0x08;
+const uint8_t COMMAND_SMETER_REPORT  = 0x53; // [COMMAND_SMETER_REPORT(Rssi)]
+const uint8_t COMMAND_PHYS_PTT_DOWN  = 0x44; // [COMMAND_PHYS_PTT_DOWN()]
+const uint8_t COMMAND_PHYS_PTT_UP    = 0x55; // [COMMAND_PHYS_PTT_UP()]
+const uint8_t COMMAND_DEBUG_INFO     = 0x01; // [COMMAND_DEBUG_INFO(char[])]
+const uint8_t COMMAND_DEBUG_ERROR    = 0x02; // [COMMAND_DEBUG_ERROR(char[])]
+const uint8_t COMMAND_DEBUG_WARN     = 0x03; // [COMMAND_DEBUG_WARN(char[])]
+const uint8_t COMMAND_DEBUG_DEBUG    = 0x04; // [COMMAND_DEBUG_DEBUG(char[])]
+const uint8_t COMMAND_DEBUG_TRACE    = 0x05; // [COMMAND_DEBUG_TRACE(char[])]
+const uint8_t COMMAND_HELLO          = 0x06; // [COMMAND_HELLO()]
+const uint8_t COMMAND_RX_AUDIO       = 0x07; // [COMMAND_RX_AUDIO(int8_t[])]
+const uint8_t COMMAND_VERSION        = 0x08; // [COMMAND_VERSION(Version)]
 
 // COMMAND_VERSION parameters
 struct version {
