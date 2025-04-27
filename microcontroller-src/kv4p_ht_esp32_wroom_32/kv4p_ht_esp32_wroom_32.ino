950ddd3038519798d74f4aa33095410391934bf7
Vance Vagell <vance@vagell.com>
2025-04-26 20:53:33 -0400

    Prepare to release v1.8.1 of Android app and v14 of firmware.

--- microcontroller-src/kv4p_ht_esp32_wroom_32/kv4p_ht_esp32_wroom_32.ino
+++ microcontroller-src/kv4p_ht_esp32_wroom_32/kv4p_ht_esp32_wroom_32.ino
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "buttons.h"
 #include "utils.h"
 
-const uint16_t FIRMWARE_VER = 13;
+const uint16_t FIRMWARE_VER = 14;
 
 const uint32_t RSSI_REPORT_INTERVAL_MS = 100;
 const uint16_t USB_BUFFER_SIZE = 1024*2;
